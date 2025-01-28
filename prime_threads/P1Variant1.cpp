888eef26b8ae255a95e30594dd4beab465e71540
Naman Srivastava <83742598+houin679@users.noreply.github.com>
2025-01-28 09:22:02 +0800

    Update P1Variant1.cpp

--- prime_threads/P1Variant1.cpp
+++ prime_threads/P1Variant1.cpp
@@ -64,3 +64,32 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+#include "A1.h"
+#include "prime_operations.h"
+
+/*
+* This function looks for the prime numbers within a specific range and prints them immediately.
+* @param X the number of threads to be created
+* @param y the range of numbers to be searched
+* @param mtx the mutex to be used for synchronization
+*/
+void taskA1(int X, int y, mutex& mtx) {
+    vector<thread> threads;
+    int step = y / X;
+
+    for (int i = 0; i < X; ++i) {
+        int start = i * step + 1;
+        int end = (i == X - 1) ? y : (i + 1) * step;
+        threads.emplace_back(search_primes, start, end, i + 1, ref(mtx));
+    }
+
+    for (auto& th : threads) {
+        th.join();
+    }
+}
